39ef6f5ff25f22b1a10493c18b53d00b3717832b
Feltlin <82766584+Feltlin@users.noreply.github.com>
2024-11-30 02:13:08 -0500

    Time: 0 ms (100%), Space: 41.9 MB (67.5%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,23 @@
+class Solution {
+    public double myPow(double x, int n) {
+        double power = 1;
+        long m = n;
+        if(m == 0){
+            return 1;
+        }
+
+        if(m < 0){
+            x = 1/x;
+            m = -m;
+        }
+
+        while(m > 0){
+            if(m%2 == 1){
+                power *= x;
+            }
+            x *= x;
+            m /= 2;
+        }
+    return power;
+    }
+}

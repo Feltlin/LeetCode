c3993da655a279d82f689e9fb497f00ba3eda1d0
Feltlin <82766584+Feltlin@users.noreply.github.com>
2025-05-30 00:09:03 -0400

    Time: 0 ms (100%), Space: 17.8 MB (70.62%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.py
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -1,9 +1,5 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        count = {}
-        for i in s:
-            count[i] = count.get(i, 0) + 1
         for i in t:
-            count[i] = count.get(i, 0) - 1
-            if count[i] == -1:
+            if t.count(i) > s.count(i):
                 return i

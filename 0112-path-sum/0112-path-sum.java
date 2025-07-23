e7e01ec380bfe72736a363047e09c52192bd7e25
Feltlin <82766584+Feltlin@users.noreply.github.com>
2025-07-22 20:33:17 -0400

    Time: 0 ms (100%), Space: 43.4 MB (25.48%) - LeetHub

--- 0112-path-sum/0112-path-sum.java
+++ 0112-path-sum/0112-path-sum.java
@@ -18,7 +18,7 @@ class Solution {
         if(root == null){
             return false;
         }
-        if(root.val == targetSum){
+        if(root.val == targetSum && root.left == null && root.right == null){
             return true;
         }
         return hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right, targetSum - root.val);

b3de6965241a9955d90e82d4504068138ee67cc7
Feltlin <82766584+Feltlin@users.noreply.github.com>
2025-10-15 16:30:08 -0400

    Time: 5 ms (50.99%), Space: 44.8 MB (21.02%) - LeetHub

---
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean judgeCircle(String moves) {
+        int x = 0;
+        int y = 0;
+        for(int i = 0; i < moves.length(); ++i){
+            switch(moves.charAt(i)){
+                case 'R':
+                    ++x;
+                    break;
+                case 'L':
+                    --x;
+                    break;
+                case 'U':
+                    ++y;
+                    break;
+                case 'D':
+                    --y;
+                    break;
+            }
+        }
+        if(x == 0 && y == 0){
+            return true;
+        }
+        return false;
+    }
+}
